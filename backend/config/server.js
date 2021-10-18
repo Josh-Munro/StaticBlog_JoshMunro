852392b5341b4be35c33d3fa00dad6fa0fa69caa
Josh <josh-munro@live.nmit.ac.nz>
2021-10-19 10:04:49 +1300

    Made blog

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1631466eb378dadd6874ccc094d0d43'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '90072ee5-5f52-47ef-8450-5595a6e81dd8'
+};
